5e0adda671edcc77665b3e4ddf945436244032ab
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-04-25 09:46:24 -0500

    Default spring-boot project added

---
+++ clean-architecture/src/test/java/tech/edev404/cleanarchitecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.edev404.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
